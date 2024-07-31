8330718c930cf303c5ffe837303f06d721d61b96
sudhanv09 <mystogun125@gmail.com>
2024-07-31 13:08:32 +0800

    project init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <div className={styles.center}>
+        <h1>Hello</h1>
+      </div>
+    </main>
+  );
+}
